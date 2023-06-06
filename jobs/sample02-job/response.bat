b6957d71db9e8709919af715ced78ea05073e613
Yu-Hsuan Haung <Yu-Hsuan.Huang@itsp-inc.com>
2023-06-06 16:18:21 -0400

    update

--- jobs/sample02-job/response.bat
+++ jobs/sample02-job/response.bat
@@ -1,8 +1,10 @@
-SET "URL=https://www.example.com/
+:: SET "URL=https://www.example.com/
 
-SET HTTP=
-for /f %%a in ( '"%SCRIPT_DIR%\curl" -s -o nul -v -X POST -w "%%{http_code}" "%URL%"' ) do set HTTP=%%a
+:: SET HTTP=
+:: for /f %%a in ( '"%SCRIPT_DIR%\curl" -s -o nul -v -X POST -w "%%{http_code}" "%URL%"' ) do set HTTP=%%a
 
+@echo off
+SET HTTP=200
 if "%HTTP%" == "200" (
     echo successed
     exit /b 0
